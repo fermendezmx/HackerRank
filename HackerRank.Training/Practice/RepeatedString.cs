d46b0b58525fda090b49582d313fb8e73de93b64
Fernando Méndez <mendezc.jf@gmail.com>
2020-06-30 22:12:58 -0500

    [Solved] Repeated String

---
+++ HackerRank.Training/Practice/RepeatedString.cs
@@ -0,0 +1,38 @@
+﻿namespace HackerRank.Training.Practice
+{
+    public static class RepeatedString
+    {
+        public static long Solution(string s, long n)
+        {
+            long result = 0;
+            int A = 0;
+
+            // Count how many 'a's are in the string
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == 'a')
+                {
+                    A++;
+                }
+            }
+
+            // Gives a no decimal value
+            long times = n / s.Length;
+            // Here we get the residual
+            long diff = n - (s.Length * times);
+            // And calculate how many times should be an 'a'
+            result = A * times;
+
+            // Then look for more 'a's in the end
+            for (int i = 0; i < diff; i++)
+            {
+                if (s[i] == 'a')
+                {
+                    result++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ HackerRank.UnitTest/Practice/TRepeatedString.cs
@@ -0,0 +1,30 @@
+using HackerRank.Training.Practice;
+using Xunit;
+
+namespace HackerRank.UnitTest
+{
+    public class TRepeatedString
+    {
+        [Fact]
+        public void Test1()
+        {
+            long n = 10;
+            string s = "aba";
+            long expected = 7;
+            long result = RepeatedString.Solution(s, n);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            long n = 1000000000000;
+            string s = "a";
+            long expected = 1000000000000;
+            long result = RepeatedString.Solution(s, n);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
