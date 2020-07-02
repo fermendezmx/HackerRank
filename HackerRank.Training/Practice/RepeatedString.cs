1f258d1527419dcaa692386dce3fa829563f1fc5
Fernando Méndez <mendezc.jf@gmail.com>
2020-07-01 20:23:21 -0500

    Updating comments in RepeatedString.cs

--- HackerRank.Training/Practice/RepeatedString.cs
+++ HackerRank.Training/Practice/RepeatedString.cs
@@ -7,7 +7,7 @@
             long result = 0;
             int A = 0;
 
-            // Count how many 'a's are in the string
+            // Count how many 'a's are in the given string 's'
             for (int i = 0; i < s.Length; i++)
             {
                 if (s[i] == 'a')
@@ -16,12 +16,14 @@
                 }
             }
 
-            // Gives a no decimal value
+            // Get the number of times that the given string 's' should
+            // be placed in a new string of 'n' items
             long times = n / s.Length;
-            // Here we get the residual
-            long diff = n - (s.Length * times);
-            // And calculate how many times should be an 'a'
+            // Calculate how many 'a's we had found until now
             result = A * times;
+            // Given that 'times' may leave a residual
+            // We check if there are some missing chars to fill a string of 'n' items
+            long diff = n - (s.Length * times);
 
             // Then look for more 'a's in the end
             for (int i = 0; i < diff; i++)
