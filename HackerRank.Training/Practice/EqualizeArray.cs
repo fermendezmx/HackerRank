0d5211d337cc1ef07cb4a599db5fb77c3e58e0d6
Fernando Méndez <mendezc.jf@gmail.com>
2020-07-01 19:54:36 -0500

    [Solved] Equalize the Array

---
+++ HackerRank.Training/Practice/EqualizeArray.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+
+namespace HackerRank.Training.Practice
+{
+    public static class EqualizeArray
+    {
+        public static int Solution(int[] arr)
+        {
+            int result = 0;
+            int counter = 0;
+            int removed = 0;
+            // Get the list of individual numbers availables in array
+            int[] numbers = arr.Distinct().ToArray();
+
+            // Look over the list of individual numbers in array
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                // Count how many times is repeated the current number in the original array
+                counter = arr.Count(x => x == numbers[i]);
+                // The rest of the numbers in the original array can be removed
+                removed = arr.Length - counter;
+
+                // Check if the current value of 'removed' is lower than a previous one
+                if (result == 0 || removed < result)
+                {
+                    // Then update the result to a lower number of removed items:
+                    // This set the minimum number of elements to delete
+                    result = removed;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ HackerRank.UnitTest/Practice/TEqualizeArray.cs
@@ -0,0 +1,28 @@
+using HackerRank.Training.Practice;
+using Xunit;
+
+namespace HackerRank.UnitTest
+{
+    public class TEqualizeArray
+    {
+        [Fact]
+        public void Test1()
+        {
+            int[] input = { 3, 3, 2, 1, 3 };
+            long expected = 2;
+            long result = EqualizeArray.Solution(input);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            int[] input = { 1, 2, 3, 1, 2, 3, 3, 3 };
+            long expected = 4;
+            long result = EqualizeArray.Solution(input);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
