940219281a5d6328cfd58fc3dc0b1ddc944a1c8c
Fernando Méndez <mendezc.jf@gmail.com>
2020-07-03 23:18:36 -0500

    [Solved] 2D Array - DS (maximum hourglass sum)

---
+++ HackerRank.Training/Practice/HourglassSum.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRank.Training.Practice
+{
+    public static class HourglassSum
+    {
+        public static int Solution(int[][] arr)
+        {
+            List<int> result = new List<int>();
+
+            // Loop over the 2D array to get the sum of every hourglass
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    // Get the hourglass sum for the current position
+                    //            A             B              C
+                    int sum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] +
+                    //       D
+                    arr[i + 1][j + 1] +
+                    //     E                F                  G
+                    arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+
+                    result.Add(sum);
+                }
+            }
+
+            // Get the maximum hourglass sum
+            return result.Max();
+        }
+    }
+}
---
+++ HackerRank.UnitTest/Practice/THourglassSum.cs
@@ -0,0 +1,56 @@
+using HackerRank.Training.Practice;
+using Xunit;
+
+namespace HackerRank.UnitTest
+{
+    public class THourglassSum
+    {
+        [Fact]
+        public void Test1()
+        {
+            int[][] input = new int[6][];
+            input[0] = new int[] { 1, 1, 1, 0, 0, 0 };
+            input[1] = new int[] { 0, 1, 0, 0, 0, 0 };
+            input[2] = new int[] { 1, 1, 1, 0, 0, 0 };
+            input[3] = new int[] { 0, 0, 2, 4, 4, 0 };
+            input[4] = new int[] { 0, 0, 0, 2, 0, 0 };
+            input[5] = new int[] { 0, 0, 1, 2, 4, 0 };
+            long expected = 19;
+            long result = HourglassSum.Solution(input);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            int[][] input = new int[6][];
+            input[0] = new int[] { 1, 1, 1, 0, 0, 0 };
+            input[1] = new int[] { 0, 1, 0, 0, 0, 0 };
+            input[2] = new int[] { 1, 1, 1, 0, 0, 0 };
+            input[3] = new int[] { 0, 9, 2, -4, -4, 0 };
+            input[4] = new int[] { 0, 0, 0, -2, 0, 0 };
+            input[5] = new int[] { 0, 0, -1, -2, -4, 0 };
+            long expected = 13;
+            long result = HourglassSum.Solution(input);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            int[][] input = new int[6][];
+            input[0] = new int[] { -9, -9,  -9, 1, 1, 1 };
+            input[1] = new int[] { 0, -9, 0, 4, 3, 2 };
+            input[2] = new int[] { -9, -9, -9, 1, 2, 3 };
+            input[3] = new int[] { 0, 0, 8, 6, 6, 0 };
+            input[4] = new int[] { 0, 0, 0, -2, 0, 0 };
+            input[5] = new int[] { 0, 0, 1, 2, 4, 0 };
+            long expected = 28;
+            long result = HourglassSum.Solution(input);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
