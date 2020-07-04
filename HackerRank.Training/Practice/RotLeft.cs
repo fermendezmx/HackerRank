482b07a1e293703e26fcbfe0b6dd709356e15ece
Fernando Méndez <mendezc.jf@gmail.com>
2020-07-04 00:19:12 -0500

    [Solved] Arrays: Left Rotation

---
+++ HackerRank.Training/Practice/RotLeft.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRank.Training.Practice
+{
+    public static class RotLeft
+    {
+        public static int[] Solution(int[] a, int d)
+        {
+            int position;
+            int n = a.Length;
+            int[] result = new int[n];
+
+            // Look over the array to rotate every number
+            for (int i = 0; i < n; i++)
+            {
+                // Calculate new position for current number
+                position = i - d;
+
+                // Fix position if it is lower than 0
+                if (position < default(int))
+                {
+                    position = n + position;
+                }
+
+                // Assign number to the new position
+                result[position] = a[i];
+            }
+
+            return result;
+        }
+    }
+}
---
+++ HackerRank.UnitTest/Practice/TRotLeft.cs
@@ -0,0 +1,30 @@
+using HackerRank.Training.Practice;
+using Xunit;
+
+namespace HackerRank.UnitTest
+{
+    public class TRotLeft
+    {
+        [Fact]
+        public void Test1()
+        {
+            int d = 4;
+            int[] a = { 1, 2, 3, 4, 5 };
+            int[] expected = { 5, 1, 2, 3, 4 };
+            int[] result = RotLeft.Solution(a, d);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            int d = 10;
+            int[] a = { 41, 73, 89, 7, 10, 1, 59, 58, 84, 77, 77, 97, 58, 1, 86, 58, 26, 10, 86, 51 };
+            int[] expected = { 77, 97, 58, 1, 86, 58, 26, 10, 86, 51, 41, 73, 89, 7, 10, 1, 59, 58, 84, 77 };
+            int[] result = RotLeft.Solution(a, d);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
