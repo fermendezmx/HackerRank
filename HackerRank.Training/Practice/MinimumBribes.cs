00399796733e0501dacdce8c28f1826adbe2d2ee
Fernando Méndez <mendezc.jf@gmail.com>
2023-10-13 14:09:54 -0700

    Update MinimumBribes.cs

--- HackerRank.Training/Practice/MinimumBribes.cs
+++ HackerRank.Training/Practice/MinimumBribes.cs
@@ -2,41 +2,40 @@
 {
     public static class MinimumBribes
     {
-        public static string Solution(int[] q)
+        public static string Solution(List<int> queue)
         {
             int result = 0;
-            int n = q.Length;
+            int n = queue.Count;
             // The person will be holding his initial position
             int person,
             // This position will point where are we placed in the queue
             position,
             // This min variable will save the lowest original position found in the queue
             min = n;
+            
             //1 2 5 3 7 8 6 4
             for (int i = n - 1; i >= 0; i--)
             {
-                person = q[i];
+                person = queue[i];
                 position = i + 1;
 
-                // If the person was moved forward more than 3 positions
-                if (person - i > 3)
-                {
-                    // Then is chaotic
-                    return "Too chaotic";
-                }
                 // If the person was moved forward to the current position
-                else if (person > position)
-                {
+                if (person > position) {
                     // Then we count the number of positions that the person was moved
                     result += person - position;
-                }
-                else
-                {
-                    // If we find a lowest ticket
+                    
+                    // If the person was moved forward more than 2 positions
+                    if (person - position > 2) {
+                        // Then is chaotic
+                        Console.WriteLine("Too chaotic");
+                        return;
+                    }
+                } else {
+                    // If we find a lower ticket
                     if (min > person)
                     {
                         // Not aditional bribes
-                        // But set the min to the new lowest found
+                        // but set the min to the new lowest found
                         min = person;
                     }
                     // If the person was moved
@@ -47,8 +46,8 @@
                     }
                 }
             }
-
-            return $"{result}";
+            
+            Console.WriteLine(result);
         }
     }
 }
