5375fdd7fec8bcaaedfd2d7a490e1fc9e1de92d7
Fernando Méndez <mendezc.jf@gmail.com>
2023-10-15 20:47:44 -0700

    Submit Sherlock and Anagrams

---
+++ HackerRank.Training/Practice/SherlockAndAnagrams.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace HackerRank.Training.Practice
+{
+    public static class SherlockAndAnagrams
+    {
+        public static int Solution(string input)
+        {
+            int level = 0;
+            int result = 0;
+            Hashtable table = new Hashtable();
+
+            // Nested loop to get all the combinations of substrings in the input
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = 0; j < input.Length - level; j++)
+                {
+                    // Get the characters of the substring for each combination
+                    char[] characters = input.Substring(j, level + 1).ToArray();
+                    // Sort to compare the string with others with the exact same content
+                    Array.Sort(characters);
+                    // Create the string for the characters
+                    string word = new string(characters);
+
+                    if (!table.ContainsKey(word))
+                        table.Add(word, 0);
+
+                    // Count the times the string is repeated
+                    table[word] = (int)table[word] + 1;
+                }
+
+                // This keep track of the length of substrings
+                level++;
+            }
+
+            foreach (var key in table.Keys)
+            {
+                int n = (int)table[key];
+
+                if (n > 1)
+                    // Calculate the combinations for each string repeated
+                    result += (n - 1) * n / 2;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ HackerRank.UnitTest/Practice/TSherlockAndAnagrams.cs
@@ -0,0 +1,48 @@
+using HackerRank.Training.Practice;
+using Xunit;
+
+namespace HackerRank.UnitTest.Practice
+{
+    public class TSherlockAndAnagrams
+    {
+        [Fact]
+        public void Test1()
+        {
+            string input = "abba";
+            int expected = 4;
+            int result = SherlockAndAnagrams.Solution(input);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            string input = "abcd";
+            int expected = 0;
+            int result = SherlockAndAnagrams.Solution(input);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            string input = "ifailuhkqq";
+            int expected = 3;
+            int result = SherlockAndAnagrams.Solution(input);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            string input = "kkkk";
+            int expected = 10;
+            int result = SherlockAndAnagrams.Solution(input);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
